3d76736ece63bed32ccc2501104611965aeb8b33
Patrick Shushereba <pshushereba@gmail.com>
2021-01-30 12:09:38 -0800

    Wired up basic file read and edit.

--- app/main.js
+++ app/main.js
@@ -1,1 +1,44 @@
-console.log('This is an Electron application!');
+const fs = require("fs");
+const { app, BrowserWindow, dialog } = require("electron");
+
+// We want to define the main browser window here and assign it in our 'ready' event listener to prevent it from getting garbage collected when the 'ready' event listener is finished running.
+
+let mainWindow = null;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({ show: false });
+
+  mainWindow.loadFile(`${__dirname}/index.html`);
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+});
+
+exports.getFileFromUser = () => {
+  const files = dialog.showOpenDialog({
+    properties: ["openFile"],
+    buttonLabel: "Unveil",
+    filters: [
+      {
+        name: "Markdown Files",
+        extensions: ["md", "mdown", "markdown", "marcdown"],
+      },
+      {
+        name: "Text Files",
+        extensions: ["txt", "text"],
+      },
+    ],
+  });
+
+  if (!files) return;
+
+  const file = files[0];
+
+  openFile(file);
+};
+
+const openFile = (file) => {
+  const content = fs.readFileSync(file).toString();
+  mainWindow.webContents.send("file-opened", file, content);
+};
--- app/renderer.js
+++ app/renderer.js
@@ -1,20 +1,66 @@
-const marked = require('marked');
+const marked = require("marked");
+const path = require("path");
+const { remote, ipcRenderer } = require("electron");
+const mainProcess = remote.require("./main");
+const currentWindow = remote.getCurrentWindow();
 
-const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
-const newFileButton = document.querySelector('#new-file');
-const openFileButton = document.querySelector('#open-file');
-const saveMarkdownButton = document.querySelector('#save-markdown');
-const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('#save-html');
-const showFileButton = document.querySelector('#show-file');
-const openInDefaultButton = document.querySelector('#open-in-default');
+let filePath = null;
+let originalContent = "";
+let isEdited = false;
 
-const renderMarkdownToHtml = markdown => {
+const markdownView = document.querySelector("#markdown");
+const htmlView = document.querySelector("#html");
+const newFileButton = document.querySelector("#new-file");
+const openFileButton = document.querySelector("#open-file");
+const saveMarkdownButton = document.querySelector("#save-markdown");
+const revertButton = document.querySelector("#revert");
+const saveHtmlButton = document.querySelector("#save-html");
+const showFileButton = document.querySelector("#show-file");
+const openInDefaultButton = document.querySelector("#open-in-default");
+
+const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
-markdownView.addEventListener('keyup', event => {
+const updateUserInterface = (isEdited) => {
+  let title = "File Sale";
+
+  if (filePath) {
+    title = `${path.basename(filePath)} - ${title}`;
+  }
+
+  if (isEdited) {
+    title = `${title} (Edited)`;
+  }
+
+  currentWindow.setRepresentedFilename(filePath);
+  currentWindow.setDocumentEdited(isEdited);
+
+  saveMarkdownButton.disabled = !isEdited;
+  revertButton.disabled = !isEdited;
+
+  console.log({ isEdited });
+
+  currentWindow.setTitle(title);
+};
+
+markdownView.addEventListener("keyup", (event) => {
   const currentContent = event.target.value;
+
   renderMarkdownToHtml(currentContent);
+  updateUserInterface(currentContent !== originalContent);
+});
+
+openFileButton.addEventListener("click", () => {
+  mainProcess.getFileFromUser();
+});
+
+ipcRenderer.on("file-opened", (event, file, content) => {
+  filePath = file;
+  originalContent = content;
+
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+
+  updateUserInterface();
 });
